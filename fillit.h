b80e581f2da6a993468b4312bf7270b5647352f9
Price Maul <pmaul@oa-a2.21-school.ru>
2020-03-05 18:33:12 +0300

    125

--- fillit.h
+++ fillit.h
@@ -1,31 +1,42 @@
-//
-// Created by Price Maul on 02/03/2020.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmaul <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 18:16:15 by pmaul             #+#    #+#             */
+/*   Updated: 2020/03/05 18:26:12 by pmaul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FILLIT_H
-#define FILLIT_H
+# define FILLIT_H
 
 # include "libft.h"
 # include <stdlib.h>
 
-typedef struct s_tetri
+typedef struct		s_tetri
 {
-    int y;
-    int x;
-    char *blockcode;
-}               t_tetri;
+	int				y;
+	int				x;
+	char			*blockcode;
+}					t_tetri;
 
-int checkblock(char *tetri, int ret);
-t_list *readnstore(int fd);
-t_tetri *encode(char *buf, char c);
-int blockletter(char *blockcode, char *buf, char c, int xyjbm[5]);
-void checkright(char *blockcode, char *buf, char c, int xyjbm[5]);
-void checkdown(char *blockcode, char *buf, char c, int xyjbm[5]);
-void checkleft(char *blockcode, char *buf, char c, int xyjbm[5]);
-char **fillit(t_list *blocks);
-char **square(int size);
-void place(char **square, char *blockcode, int y, int x);
-void erase(char **square, char *blockcode, int y, int x);
-void disp_table(char **table);
+int					checkblock(char *tetri, int ret);
+t_list				*readnstore(int fd);
+t_tetri				*encode(char *buf, char c);
+int					blockletter(char *blockcode, char *buf, char c, \
+						int xyjbm[5]);
+void				checkright(char *blockcode, char *buf, char c, \
+						int xyjbm[5]);
+void				checkdown(char *blockcode, char *buf, char c, int xyjbm[5]);
+void				checkleft(char *blockcode, char *buf, char c, int xyjbm[5]);
+char				**fillit(t_list *blocks);
+char				**square(int size);
+void				place(char **square, char *blockcode, int y, int x);
+void				erase(char **square, char *blockcode, int y, int x);
+
+void				disp_table(char **table);
 
 #endif
--- readnstore.c
+++ readnstore.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readnstore.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmaul <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 18:20:52 by pmaul             #+#    #+#             */
+/*   Updated: 2020/03/05 18:20:59 by pmaul            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -6,43 +18,43 @@
 
 #define BUF_SIZE 21
 
-static void creatlistfirst(t_list **fb, t_list **blocks, char *buf, int *i)
+static void		creatlistfirst(t_list **fb, t_list **blocks, char *buf, int *i)
 {
-    *fb = ft_lstnew(encode(buf, 'A' + *i), sizeof(t_tetri));
-    *blocks = *fb;
+	*fb = ft_lstnew(encode(buf, 'A' + *i), sizeof(t_tetri));
+	*blocks = *fb;
 }
 
-static void creatlist(t_list **blocks, t_list **next, char *buf, int *i)
+static void		creatlist(t_list **blocks, t_list **next, char *buf, int *i)
 {
-    *next = ft_lstnew(encode(buf, 'A' + *i), sizeof(t_tetri));
-    *blocks = *next;
+	*next = ft_lstnew(encode(buf, 'A' + *i), sizeof(t_tetri));
+	*blocks = *next;
 }
 
-t_list      *readnstore(int fd)
+t_list			*readnstore(int fd)
 {
-    int     ret;
-    int     i;
-    char    buf[BUF_SIZE + 1];
-    t_list  *blocks;
-    t_list  *firstblock;
+	int			ret;
+	int			i;
+	char		buf[BUF_SIZE + 1];
+	t_list		*blocks;
+	t_list		*firstblock;
 
-    i = 0;
-    firstblock = NULL;
-    while ((ret = read(fd, buf, BUF_SIZE)))
-    {
-        buf[ret] = '\0';
-        if (!(checkblock(buf, ret)))
-            return (NULL);
-        else
-        {
-            if (firstblock == NULL)
-                creatlistfirst(&firstblock, &blocks, buf, &i);
-            else
-                creatlist(&blocks, &blocks->next, buf, &i);
-            i++;
-        }
-    }
-    if (ret == 0 && buf[20] != '\0')
-        return (NULL);
-    return (firstblock);
+	i = 0;
+	firstblock = NULL;
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		buf[ret] = '\0';
+		if (!(checkblock(buf, ret)))
+			return (NULL);
+		else
+		{
+			if (firstblock == NULL)
+				creatlistfirst(&firstblock, &blocks, buf, &i);
+			else
+				creatlist(&blocks, &blocks->next, buf, &i);
+			i++;
+		}
+	}
+	if (ret == 0 && buf[20] != '\0')
+		return (NULL);
+	return (firstblock);
 }
